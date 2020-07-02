c365ef9f034c9c077729b87f6311b3570e0c25f0
PM <pm1975@windowslive.com>
2020-07-02 13:19:39 +0200

    Lesson 5

--- PierwszyProgram/PierwszyProgram/Program.cs
+++ PierwszyProgram/PierwszyProgram/Program.cs
@@ -6,7 +6,16 @@ namespace PierwszyProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var nazwa1 = 5;
+            var nazwa2 = "Kurs programowania C#";
+
+            var nazwa3 = 5.3;
+
+            nazwa1 = 7;
+            nazwa2 = "Drugi kurs";
+
+            Console.WriteLine(nazwa1);
+            Console.WriteLine(nazwa2);
         }
     }
 }
