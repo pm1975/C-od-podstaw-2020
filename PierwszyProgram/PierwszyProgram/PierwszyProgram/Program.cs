4811343dcf0d8405fc3b2e4024c269bc492bb70a
PM <pm1975@windowslive.com>
2020-07-03 16:34:20 +0200

    12 IntelliSense (do 3 min)

---
+++ PierwszyProgram/PierwszyProgram/PierwszyProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PierwszyProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lista = new List<int>();
+
+            //lista.
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
