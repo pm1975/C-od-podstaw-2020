73c6b7bafdd5937bb41b285d717a6471646b3203
PM <pm1975@windowslive.com>
2020-07-13 14:41:51 +0200

    12 IntelliSense - koniec

--- PierwszyProgram/PierwszyProgram/Program.cs
+++ PierwszyProgram/PierwszyProgram/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PierwszyProgram
 {
@@ -6,46 +7,15 @@ namespace PierwszyProgram
     {
         static void Main(string[] args)
         {
-            //double liczba = 8.12312;
-            //NazwaFunkcji();
-            //var liczba2 = PodwojLiczbe(PrzekonwertujDoubleNaInta(liczba));
-            //Console.WriteLine(liczba2);
-            var liczba = 5;
-            Console.WriteLine(liczba);
-            Console.WriteLine(PodwojLiczbe(liczba));
-            Console.WriteLine(liczba);
-            Console.WriteLine(PodwojLiczbe1(out liczba));
-            Console.WriteLine(liczba);
+            var lista = new List<int>();
+
+            var slownik = new Dictionary<string, int>();
+
+            var dataWKalendarzu = new DateTime(2020, 7, 13, 14, 38, 0);          
+
+            Console.WriteLine(dataWKalendarzu.Date);
+            Console.WriteLine(dataWKalendarzu.ToLongDateString());
 
         }
-
-        private static void NazwaFunkcji()
-        {
-            Console.WriteLine(5);
-        }
-
-        private static string PodwojLiczbe(int liczba)
-        {
-            return (liczba * 2).ToString();
-        }
-
-        private static int PodwojLiczbe1(out int liczba)
-        {
-            liczba = 2;
-            return liczba * 2;
-        }
-
-        private static int PrzekonwertujDoubleNaInta(double liczba)
-        {
-            if (int.TryParse(liczba.ToString(), out int liczba2))
-            {
-                return liczba2;
-            }
-            else
-            {
-                return 0;
-            }
-        }
-
     }
 }
