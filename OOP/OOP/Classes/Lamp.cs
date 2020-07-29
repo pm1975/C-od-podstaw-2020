300353064264f38114d66dd3813cfaaa7f6fca5e
PM <pm1975@windowslive.com>
2020-07-29 16:04:54 +0200

    23. Gettery i settery. Do 4.00

--- OOP/OOP/Classes/Lamp.cs
+++ OOP/OOP/Classes/Lamp.cs
@@ -6,7 +6,7 @@ namespace OOP.Classes
 {
     class Lamp : Item
     {
-        public int Power { get; set; }
+        public int Power { get; private set; }
 
         public void LightItself()
         {
