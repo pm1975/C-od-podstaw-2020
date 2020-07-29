30e01ec7819e4c92cd90bb8c14f926444aea4b13
PM <pm1975@windowslive.com>
2020-07-29 14:15:39 +0200

    19. Klasy. Do 5.30

---
+++ OOP/OOP/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            MyClass myClassObject = new MyClass();
+
+            myClassObject.Function2();
+            MyClass.Function1();
+        }
+    }
+
+    class MyClass
+    {
+        public static void Function1()
+        {
+
+        }
+
+        public void Function2()
+        {
+
+        }
+    }
+}
