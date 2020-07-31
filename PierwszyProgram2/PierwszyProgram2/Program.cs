4587a65cf51ac265a17d5b9b54c060c85aef2956
pm1975 <pm1975@windowslive.com>
2020-07-31 21:39:44 +0200

    20200731 2139 mac

---
+++ PierwszyProgram2/PierwszyProgram2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PierwszyProgram2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
